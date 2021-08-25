f659e160e5d23e7bbf56ec631f085c316f9594b2
mmskazak <mmskazak@gmail.com>
2021-08-25 13:11:00 +0300

    upd

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,11 @@
 require('./bootstrap');
 
 import Vue from 'vue';
+import App from './components/App';
 
 Vue.component('app', require('~/components/App').default);
 
 const app = new Vue({
     el:'#app',
+    App
 });
